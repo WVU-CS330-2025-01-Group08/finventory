b9c7ebfcad74c945e9ae87c2c88362c3ad9fe3c1
Oliver Lay <oklay252@gmail.com>
2025-03-31 13:18:34 -0400

    Added wv county layers and some map properties

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css'
-import Map from './Map.js';
+import { WVMap as Map } from './Map.js';
 
 function Home() {
   return (
--- client/src/Map.js
+++ client/src/Map.js
@@ -1,10 +1,9 @@
-import React, { useEffect } from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import React, { useEffect, useState } from 'react';
+import { MapContainer, TileLayer, useMap, GeoJSON } from 'react-leaflet';
 import * as L from 'leaflet';
-import * as esri from 'esri-leaflet';
 import 'leaflet/dist/leaflet.css';
 
-// Fix the Leaflet icon issue in React
+// Fix Leaflet icon issues
 delete L.Icon.Default.prototype._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
@@ -12,37 +11,83 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
-// ArcGIS feature layer component
-function FeatureLayer() {
+// County layer using direct GeoJSON fetch
+function CountyLayer() {
   const map = useMap();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
   
   useEffect(() => {
-    // WV County Boundaries Feature Service URL
-    const countyLayer = esri.featureLayer({
-      url: 'https://services.arcgis.com/BG6nSlhZSAWtExvp/arcgis/rest/services/WV_County_Boundaries/FeatureServer/0',
-      style: function() {
-        return {
-          color: '#3388ff',
-          weight: 2,
-          fillOpacity: 0.1
-        };
-      }
-    }).addTo(map);
-
-    // Add popup with county name
-    countyLayer.bindPopup(function(layer) {
-      return `<b>${layer.feature.properties.NAME} County</b>`;
-    });
-
-    // Fit bounds to WV
-    map.setView([38.9, -80.5], 7);
+    console.log("Fetching WV county data...");
     
+    // GeoJSON fetch
+    fetch('https://services.wvgis.wvu.edu/arcgis/rest/services/Boundaries/wv_political_boundary/MapServer/0/query?where=1%3D1&outFields=*&outSR=4326&f=geojson')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`HTTP error ${response.status}`);
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log("County data loaded:", data);
+        setLoading(false);
+        
+        // Create counties with more visible styling
+        const countyLayer = L.geoJSON(data, {
+            style: {
+              color: '#6B8EB9',       // Lighter blue for borders
+              weight: 1.5,            // Slightly thinner lines
+              fillColor: '#A9C6E8',   // Very light blue fill
+              fillOpacity: 0.15       // More transparent fill
+            },
+            onEachFeature: (feature, layer) => {
+              const props = feature.properties;
+              const countyName = props.County_Name_Modified || props.County_Name;
+              if (countyName) {
+                layer.bindPopup(`<b>${countyName}</b>`);
+                
+                // Add hover effects
+                layer.on({
+                  mouseover: function() {
+                    this.setStyle({
+                      fillOpacity: 0.4,
+                      weight: 2.5,
+                      color: '#0056b3'
+                    });
+                    this.bringToFront();
+                  },
+                  mouseout: function() {
+                    countyLayer.resetStyle(this);
+                  },
+                  click: function() {
+                    map.fitBounds(this.getBounds());
+                  }
+                });
+              }
+            }
+          }).addTo(map);
+          map.fitBounds(countyLayer.getBounds(), {
+            padding: [20, 20], //20px padding
+            maxZoom: 8         
+          });
+      })
+      
+      .catch(err => {
+        console.error("Failed to load counties:", err);
+        setError(err.message);
+        setLoading(false);
+        
+      });
+      
     return () => {
-      map.removeLayer(countyLayer);
     };
   }, [map]);
   
-  return null;
+  return loading ? 
+    <div className="map-loading">Loading counties...</div> : 
+    error ? 
+    <div className="map-error">Error: {error}</div> : 
+    null;
 }
 
 function WVMap() {
@@ -50,18 +95,21 @@ function WVMap() {
     <div className="map-container">
       <MapContainer 
         style={{ height: '400px', width: '100%', borderRadius: '8px' }}
-        center={[38.9, -80.5]} 
-        zoom={7} 
-        scrollWheelZoom={false}
+        center={[39.0, -80.25]}
+        zoom={7}
+        scrollWheelZoom={true}
+        minZoom={7}
+        maxZoom={10}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <FeatureLayer />
+        <CountyLayer />
       </MapContainer>
     </div>
   );
 }
 
 export default WVMap;
+export { WVMap };
