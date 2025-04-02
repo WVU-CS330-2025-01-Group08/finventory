34e4778ca6d081293d8b1057f55e52bcec2d009c
Oliver Lay <oklay252@gmail.com>
2025-04-02 13:18:10 -0400

    feat: added map boundaries to prevent scrolling outside of west virginia

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -12,6 +12,34 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
 });
 
+// Sets the map boundaries to these two corners
+const WV_BOUNDS = [
+  [37.2, -82.6], // Southwest corner
+  [40.6, -77.7]  // Northeast corner
+];
+
+function MapBoundaries() {
+  const map = useMap();
+  
+  useEffect(() => {
+    // Set hard bounds
+    map.setMaxBounds(WV_BOUNDS);
+    
+    // Make bounds "sticky" - 1.0 means maximum stickiness
+    map.options.maxBoundsViscosity = 1.0;
+    
+    // Disable world wrapping (prevents scrolling to see the same map repeated)
+    map.options.worldCopyJump = false;
+    map.options.continuousWorld = true;
+    
+    return () => {
+      // Cleanup if component unmounts
+      map.setMaxBounds(null);
+    };
+  }, [map]);
+  
+  return null;
+}
 
 function WVMap() {
   return (
@@ -23,12 +51,16 @@ function WVMap() {
         scrollWheelZoom={true}
         minZoom={7}
         maxZoom={10}
+        maxBounds={WV_BOUNDS}
+        maxBoundsViscosity={1.0}
+        worldCopyJump={false}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <CountyLayer />
+        <MapBoundaries />
       </MapContainer>
     </div>
   );
